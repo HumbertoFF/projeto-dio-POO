1cb19b6670c5dfd60fce6f3b21dd1f4ade320fb3
Humberto Freitas <95453824+HumbertoFF@users.noreply.github.com>
2023-04-23 23:28:41 -0300

    Criado classes Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Curso curso = new Curso();
+    curso.setTitulo("curso java");
+    curso.setDescricao("descricao do curso");
+    curso.setCargaHoraria(10);
+
+    Curso segCurso = new Curso();
+    segCurso.setTitulo("segundo curso");
+    segCurso.setDescricao("descrição do segundo curso");
+    segCurso.setCargaHoraria(12);
+
+    System.out.println(curso);
+    System.out.println(segCurso);
+
+    Mentoria novaMentoria = new Mentoria();
+    novaMentoria.setTitulo("mentoria para Java");
+    novaMentoria.setDescricao("descrição da mentoria");
+    novaMentoria.setData(LocalDate.now());
+
+    System.out.println(novaMentoria);
     }
 }
